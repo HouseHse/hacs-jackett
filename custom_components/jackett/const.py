47d2d27785135ca1cef1b4e207d3f06fa74b8781
H.se <mcmax1@yandex.ru>
2025-08-17 17:11:56 +0300

    Create const.py

---
+++ custom_components/jackett/const.py
@@ -0,0 +1,6 @@
+DOMAIN = "jackett"
+CONTAINER_NAME = "jackett"
+CONFIG_PATH = "/config/jackett"
+DOWNLOADS_PATH = "/config/downloads"
+JACKETT_IMAGE = "linuxserver/jackett:latest"
+DEFAULT_TRACKERS = ["1337x", "ThePirateBay", "YTS", "EZTV"]
